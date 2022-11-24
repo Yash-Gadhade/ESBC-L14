8a0b746607efe01234d4093414fd0092c63c5e98
Yash Gadhade <yashgadhade@gmail.com>
2022-11-24 17:44:43 +0530

    minting enabled

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,18 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Post } from '@nestjs/common';
+import { Body } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
+
+export class createPaymentOrderDto {
+  secret: string;
+  value: number;
+}
+
+export class claimPaymentOrderDto {
+  secret: string;
+  address: string;
+  id: number;
+}
 
 @Controller()
 export class AppController {
@@ -13,5 +26,38 @@ export class AppController {
   @Get('block-by-hash/:hash')
   getBlockByHash(@Param('hash') hash: string) {
     return this.appService.getBlock(hash);
+  }
+
+  @Get('total-supply/:address')
+  getTotalSupply(@Param('address') address: string) {
+    return this.appService.getTotalSupply(address);
+  }
+
+  @Get('allowance')
+  getAllowance(
+    @Query('address') address: string,
+    @Query('owner') owner: string,
+    @Query('spender') spender: string,
+  ) {
+    return this.appService.getAllowance(address, owner, spender);
+  }
+
+  @Get('payment-order/:id')
+  getPaymentOrder(@Param('id') id: number) {
+    return this.appService.getPaymentOrder(id);
+  }
+
+  @Post('create-payment-order')
+  createPaymentOrder(@Body() body: createPaymentOrderDto) {
+    return this.appService.createPaymentOrder(body.secret, body.value);
+  }
+
+  @Post('claim-payment-order')
+  claimPaymentOrder(@Body() body: claimPaymentOrderDto) {
+    return this.appService.claimPaymentOrder(
+      body.id,
+      body.secret,
+      body.address,
+    );
   }
 }
